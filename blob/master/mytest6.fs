9095121cf4752010a4e7c71ddd5e7fb222244510
JmapelleDemi <48983568+JmapelleDemi@users.noreply.github.com>
2019-06-07 23:19:55 +0300

    Create mytest6.fs

---
+++ blob/master/mytest6.fs
@@ -0,0 +1,16 @@
+// 17.1 
+let rec pow = function
+ | (s,0) -> ""
+ | (s,n) -> s + pow (s,n-1) 
+ 
+// 17.2
+let rec isIthChar = fun (s,n,c) -> (string(s)).[n] = c
+
+// 17.3
+let rec occFromIth = fun (s,n,c) -> 
+  let bti = function
+   | true  ->  1
+   | false ->  0
+  match (s,n,c) with
+   | (s,l,c) when l = String.length s  -> 0
+   | (s,n,c)  -> bti((string(s)).[n] = c) + occFromIth (s,n+1,c) 
