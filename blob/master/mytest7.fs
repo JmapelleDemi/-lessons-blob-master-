f98960de57df3aa2567383f567d6a641cdcf85a3
JmapelleDemi <48983568+JmapelleDemi@users.noreply.github.com>
2019-06-14 02:46:30 +0300

    Update mytest7.fs

--- blob/master/mytest7.fs
+++ blob/master/mytest7.fs
@@ -1,10 +1,10 @@
-//20.3.1.
+// 20.3.1.
 let vat n x = x * ( float n * 0.01 ) + x
 
-//20.3.2.
+// 20.3.2.
 let unvat n x = x * 100.0 / (100.0 + float n)
 
-//20.3.3.
+// 20.3.3.
 let rec min f =
     let rec testf = function
       | n when f n = 0 -> n
