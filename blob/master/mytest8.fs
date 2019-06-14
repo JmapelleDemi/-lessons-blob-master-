b80b6a6062bca6b7e536057be398221d6e656dd6
JmapelleDemi <48983568+JmapelleDemi@users.noreply.github.com>
2019-06-14 16:08:32 +0300

    Update mytest8.fs

--- blob/master/mytest8.fs
+++ blob/master/mytest8.fs
@@ -1,9 +1,3 @@
-let curry f = 
-    let g x = h
-        let h y = f (x,y)
-        h
-    g
-    
-let uncurry g = 
-        let f (x, y) = g x y
-        f
+let curry f = fun x y -> f(x,y)
+
+let uncurry f = fun (x,y) -> f x y
