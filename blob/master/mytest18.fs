4d1ac7c1b813068895c940c3df2c239e5411b7a6
JmapelleDemi <48983568+JmapelleDemi@users.noreply.github.com>
2019-10-02 20:50:57 +0300

    Update mytest18.fs

--- blob/master/mytest18.fs
+++ blob/master/mytest18.fs
@@ -7,3 +7,18 @@ let f n =
     z    <- z * step
     step <- step + 1
   z
+
+// 47.4.2
+let fibo n =
+  if n = 0 then 0 
+  elif n = 1 then 1 else
+  let mutable first = 0
+  let mutable second = 1
+  let mutable step = 1
+  let mutable sum = first + second
+  while step < n do
+    sum     <- first + second
+    first   <- second
+    second  <- sum
+    step    <- step + 1
+  sum
