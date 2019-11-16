68591b9a880cb6fa06199934de3b7d4239729227
JmapelleDemi <48983568+JmapelleDemi@users.noreply.github.com>
2019-11-16 23:18:24 +0300

    Update mytest21.fs

--- blob/master/mytest21.fs
+++ blob/master/mytest21.fs
@@ -1,15 +1,17 @@
 // 50.2.1
-let fac_seq = seq {
-    for i in 1..n do 
-      let factorial n = 
-         let rec f x a = 
-             if x <= 1 then a 
-             else f (x - 1) (a * x) 
-         f n 1 
-      yield i }
-      
+let fac_seq =
+    let rec fac m n =
+        seq {
+            yield m * n
+            yield! fac (m * n) (n + 1)
+        }
+    seq { yield 1 ; yield 1 ; yield 2 ; yield! (fac 2 3) }
+
 // 50.2.2
-let seq_seq = seq {
-    for i in 1..n do 
-      if i % 2 = 0 then  yield (i / 2)
-      else yield ( - (i + 1) / 2) }
+let seq_seq =
+    let rec f n =
+        seq {
+            if n % 2 = 0 then yield (n / 2) else yield - (n + 1) / 2
+            yield! f (n + 1)
+        }
+    seq { yield 0; yield -1; yield 1; yield! (f 3) }
